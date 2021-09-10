08f915bb850ef9310efd2f864835ead31ac035f1
Jhonnatan Eduardo Zamudio Palacios <tecsco2013@gmail.com>
2021-09-10 06:11:32 -0500

    Se implementa temporizador para simular una demora en la respuesta del servidor

--- sc03/vistas/vistas.py
+++ sc03/vistas/vistas.py
@@ -1,6 +1,12 @@
 from flask_restful import Resource
+import time
 
 class VistaRespuesta(Resource):
 
     def get(self):
-        return 'Respuesta correcta', 200
+        t=10
+        while t:
+            time.sleep(1)
+            t -= 1
+
+        return "Respuesta correcta", 200
