68b3a91b89925b18d9d8a512f52ea8bb2556463a
Jhonnatan Eduardo Zamudio Palacios <tecsco2013@gmail.com>
2021-09-10 05:37:15 -0500

    Se implementa librerias para API Rest

--- sc03/app.py
+++ sc03/app.py
@@ -1,4 +1,8 @@
 from sc03 import create_app
+from flask_restful import Resource, Api
+from flask import Flask, request
+import requests
+
 
 app = create_app('default')
 app_context = app.app_context()
