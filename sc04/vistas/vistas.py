1c010d77441baf0c85a3b1864c3d190d207a3f09
Jhonnatan Eduardo Zamudio Palacios <tecsco2013@gmail.com>
2021-09-10 07:27:25 -0500

    Se complementa los tipos de respuesta de forma aleatoria para simular un falla en la obtencion de la respuesta

--- sc04/vistas/vistas.py
+++ sc04/vistas/vistas.py
@@ -4,8 +4,21 @@ import random
 class VistaRespuesta(Resource):
 
     def get(self):
-        option=random.randint(0, 1)
+        option=random.randint(0, 4)
         if option == 0:
-            return "Respuesta correcta", 200
+            respuesta = "Repuesta correcta"
+            cod = 200
         elif option == 1:
-            return "Respuesta incorrecta", 200
+            respuesta = "Repuesta incorrecta"
+            cod = 200
+        elif option == 2:
+            respuesta = "Repuesta con incoherencias"
+            cod = 300
+        elif option == 3:
+            respuesta = "El servidor no procesará la solicitud"
+            cod = 400
+        elif option == 4:
+            respuesta = "Error interno en el servidor"
+            cod = 500
+
+        return respuesta, cod
