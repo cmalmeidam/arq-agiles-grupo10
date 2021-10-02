e2141d9f31b0f1daccdd6c5fdba188385bb226db
Jhonnatan Eduardo Zamudio Palacios <tecsco2013@gmail.com>
2021-10-02 18:43:15 -0500

    Se implemento la logica de validación para todos los escenarios

--- validator/vistas/vistas.py
+++ validator/vistas/vistas.py
@@ -1,3 +1,4 @@
+import json
 from flask_restful import Resource
 from flask import request
 import jwt
@@ -19,5 +20,36 @@ class VistaValidacion(Resource):
         except jwt.DecodeError:
             msg = 'Token con error en decodificación'
             code = 401
+        except jwt.ExpiredSignatureError:
+            msg = 'Token expirado'
+            code = 419
+        
+        ip = decode.get('ip')[0:10]
+
+        if decode.get('usuario') == user :
+            if decode.get('rol') == "admin":
+                if ip == "186.84.21.":
+                    msg = "Usuario autorizado"
+                    code = 200
+                else:
+                    msg = "Posible suplantación de usuario"
+                    code = 429
+            elif decode.get('rol') == "consulta":
+                if ip == "186.84.35.":
+                    if microservice == "query":
+                        msg = "Usuario autorizado"
+                        code = 200
+                    else:
+                        msg = "El usuario no tiene permisos para está acción"
+                        code = 403
+                else:
+                    msg = "Posible suplantación de usuario"
+                    code = 429
+            else:
+                msg = "No tiene ningún rol asociado al usuario"
+                code = 401
+        else:
+            msg = "Posible suplantación de usuario"
+            code = 429
             
         return msg, code
