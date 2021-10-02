690f51d9355e699ca627253350961de79b5cb58b
Jhonnatan Eduardo Zamudio Palacios <tecsco2013@gmail.com>
2021-10-02 18:03:28 -0500

    Se implementa la logica para detectar errores de codificacion y firma invaldia

--- validator/vistas/vistas.py
+++ validator/vistas/vistas.py
@@ -1,10 +1,23 @@
 from flask_restful import Resource
 from flask import request
+import jwt
+import datetime
 
 class VistaValidacion(Resource):
 
     def post(self):
+        key = "secret"
         user = request.json["user"]
         token = request.json["token"]
         microservice = request.json["microservice"]
-        return 'Respuesta correcta es: ' + user, 200
+
+        try:
+            decode = jwt.decode(token, key, algorithms="HS256")
+        except jwt.InvalidSignatureError:
+            msg = 'Token con firma inválida'
+            code = 401
+        except jwt.DecodeError:
+            msg = 'Token con error en decodificación'
+            code = 401
+            
+        return msg, code
