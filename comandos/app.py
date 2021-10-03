acd94ee712b2bb3c780e125a51387ed2d45ad4f6
wamartinezq <w.martinezq@uniandes.edu.co>
2021-10-03 16:27:18 -0500

    cambios en microservico comandos

--- comandos/app.py
+++ comandos/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import jsonify
 import logging
 from flask import request
+from requests_futures.sessions import FuturesSession
 
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
@@ -14,13 +15,18 @@ app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = "super-secret"
 jwt = JWTManager(app)
 
-@app.route("/", methods=["POST"])
-@jwt_required()
-def protected():
-    
-    current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
 
+@app.route("/", methods=["GET"])
+
+def protected():
+    user = request.json["user"]
+    token = request.json["token"]
+    session = FuturesSession()
+    validacion = session.post('http://127.0.0.1:5000/',json={"user": user, "token": token, "microservice": "query"})
+    if (validacion.result().status_code != 200):
+        return "Petición no válida", 403
+    else:
+        return "Petición válida", 200
 
 if __name__ == "__main__":
     app.run()
