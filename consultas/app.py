64f2db4fc9aca8aec3d7e37d05730214bca627ee
wamartinezq <w.martinezq@uniandes.edu.co>
2021-10-03 15:11:40 -0500

    Uso de sesiones

--- consultas/app.py
+++ consultas/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import jsonify
-import logging
 from flask import request
+from requests_futures.sessions import FuturesSession
+import logging
+
 
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
@@ -9,18 +11,24 @@ from flask_jwt_extended import JWTManager
 
 logging.basicConfig(filename='consultas.log', level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
+
+
 app = Flask(__name__)
 
 app.config["JWT_SECRET_KEY"] = "super-secret"
 jwt = JWTManager(app)
 
 @app.route("/", methods=["GET"])
-@jwt_required()
-def protected():
-    
-    current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
 
+def protected():
+    user = request.json["user"]
+    token = request.json["token"]
+    session = FuturesSession()
+    validacion = session.post('http://127.0.0.1:5000/',json={"user": user, "token": token, "microservice": "query"})
+    if (validacion.result().status_code != 200):
+        return "Petición no válida", 403
+    else:
+        return "Petición válida", 200
 
 if __name__ == "__main__":
     app.run()
