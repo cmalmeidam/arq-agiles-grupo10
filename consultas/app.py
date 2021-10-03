60f42d3459a8313193cb41b2238fcfe116f105ac
wamartinezq <w.martinezq@uniandes.edu.co>
2021-10-03 14:34:00 -0500

    Microservicios comandos y consultas

---
+++ consultas/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import jsonify
+import logging
+from flask import request
+
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
+logging.basicConfig(filename='consultas.log', level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+app = Flask(__name__)
+
+app.config["JWT_SECRET_KEY"] = "super-secret"
+jwt = JWTManager(app)
+
+@app.route("/", methods=["GET"])
+@jwt_required()
+def protected():
+    
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+
+if __name__ == "__main__":
+    app.run()
